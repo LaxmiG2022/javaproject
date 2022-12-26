82a8dbde787d316dcd96ab521ffd80ad23a0c8a0
laxmigadewad <gadewadlaxmis@gmail.com>
2022-12-26 18:34:05 +0530

    second commit : added code for linkedlist

---
+++ src/com/javaproject/LinkedList1.java
@@ -0,0 +1,15 @@
+package com.javaproject;
+import java.util.LinkedList;
+public class LinkedList1 {
+
+	public static void main(String[] args) {
+		
+		LinkedList al = new LinkedList();
+	    al.add(50);
+	    al.add("ram");
+	    al.add("geeta");
+	    System.out.println(al);
+	
+	}
+
+}
