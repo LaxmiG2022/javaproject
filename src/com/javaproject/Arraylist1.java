f18f92b234f33096d57916f193b2708571a34b0d
laxmigadewad <gadewadlaxmis@gmail.com>
2022-12-26 13:35:53 +0530

    first commit : first code of arraylist

---
+++ src/com/javaproject/Arraylist1.java
@@ -0,0 +1,15 @@
+package com.javaproject;
+
+import java.util.ArrayList;
+
+public class Arraylist1 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(10);
+		al.add(50);
+		al.add(null);
+		System.out.println(al);
+	}
+}
